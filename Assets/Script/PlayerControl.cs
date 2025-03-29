1389e9ec63c58053f0a0019616864fda20095a73
Mhamee <Mhameework@gmail.com>
2025-03-29 22:44:15 +0700

    1.1errorFileFix

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class cameraFollowing : MonoBehaviour
+public class PlayerControl : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -9,21 +9,15 @@ public class PlayerController : MonoBehaviour
     private bool isAir;
     
     private float respawnDelay = 2f;
-    [SerializeField]float speed = 2.5f;
+    [SerializeField]float speed = 3f;
     [SerializeField] float jumpForce = 25f;
 
-<<<<<<< HEAD
-    [SerializeField] float speed = 5f;
-    [SerializeField] float jumpForce = 30f;
     float gravity = Physics.gravity.magnitude;
-=======
->>>>>>> parent of dc9c805 (UpdatePlayerContro)
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
-
     void Update() 
     {
         //Player movement----------------------------------------------------
@@ -31,20 +25,14 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
-<<<<<<< HEAD
         Vector3 move = new Vector3(moveX, 0, moveZ) * speed;//speed walk
         rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);//spin of player
-=======
-        Vector3 move = new Vector3(moveX, 0, moveZ) * speed;
-        rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);
->>>>>>> parent of dc9c805 (UpdatePlayerContro)
 
         if (Input.GetKey(KeyCode.Space) && isGrounded == true && isAir == false) //Jumping
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
-
     }
 
     // On ground and Air cheeck---------------------------------------------
